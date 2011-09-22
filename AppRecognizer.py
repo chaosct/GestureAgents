4bfc1d8c79eec11c0694b1b4221902213a092c6e
Carles F. Julià <carles.fernandez@upf.edu>
2011-09-22 18:20:33 +0200

    introducing AppRecognizer, so Apps can compete for gestures with Recognizers. This way when a regognizer fails, the agents acquired by him can be used by an Application.

---
+++ AppRecognizer.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from Recognizer import Recognizer, newHypothesis
+import copy
+from Agent import Agent
+from Events import Event
+
+class FakeAgent(Agent):
+    def __init__(self,original):
+        self.original_agent = original
+        Agent.__init__(self,list(original.events))
+        
+    def __getattr__(self,attrname):
+        return getattr(self.original_agent,attrname)
+        
+
+class AppRecognizer(Recognizer):
+    def __init__(self,recognizer):
+        Recognizer.__init__(self)
+
+        self.recognizer = recognizer
+        self.newAgent = Event()
+        self.eventqueue = []
+        self.register_event(recognizer.newAgent,AppRecognizer._eventNewAgent)
+        self.willenqueue = True
+    
+    @newHypothesis
+    def _eventNewAgent(self,agent):
+        self.unregister_event(self.recognizer.newAgent)
+        self.agent = self._makeAgentAgent(agent)
+        self.newAgent(self.agent)
+        self.otheragent = agent
+        if not self.agent.is_someone_subscribed():
+            self.fail()
+        else:
+            for ename,event in agent.events.iteritems():
+                def ffff(self,a,eventname=ename):
+                    self.enqueueEvent(a,eventname)
+                self.register_event(event,ffff )
+    
+    def enqueueEvent(self,a,e):
+        if not self.eventqueue:
+            self.acquire(a)
+            self.complete()
+        if self.willenqueue:
+            copyagent = copy.copy(self.agent)
+            copyagent.original_agent = copy.copy(copyagent.original_agent)
+            self.eventqueue.append((e,copyagent))
+        else:
+            self.agent.events[e](self.agent)
+    
+    def execute(self):
+        self.willenqueue = False
+        for event_name,agent in self.eventqueue:
+            self.agent.events[event_name](agent)
+        #self.finish() we deliberately don't finnish
+    
+    @staticmethod
+    def _makeAgentAgent(agent):
+        return FakeAgent(agent)
+    
+    def duplicate(self):
+        d = AppRecognizer(self.recognizer)
+        d.newAgent = self.newAgent
+        Recognizer.copy_to(self,d)
+            
