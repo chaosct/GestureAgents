64b2764cfa4441e9c22e59b08a776ef1ac8bd52d
Carles F. Julià <carles.fernandez@upf.edu>
2011-11-09 17:09:24 +0100

    We don't keep a list of loaded recognizers in GestureAgents.Gestures

--- GestureAgents/Gestures/__init__.py
+++ GestureAgents/Gestures/__init__.py
@@ -14,7 +14,7 @@ _current_module = sys.modules[__name__]
 
 _modules = [__name__+'.'+os.path.splitext(f)[0] for f in os.listdir(_DIR) if f.endswith('.py') and os.path.splitext(f)[0] != os.path.splitext(_FNAME)[0]]
 recognizers = []
-recognizers_loaded = {}
+recognizers_loaded = []
 
 for mm in _modules:
     __import__(mm)
@@ -31,5 +31,6 @@ def load_all():
 
 def load_recognizer(r):
     if r not in recognizers_loaded:
-        recognizers_loaded[r]=r()
-    return recognizers_loaded[r]
+        recognizers_loaded.append(r)
+        r()
+    #return recognizers_loaded[r]
