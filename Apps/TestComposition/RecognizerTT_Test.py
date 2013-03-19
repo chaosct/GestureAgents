3887508cc27a665ee73bd00bc87a8ac453270263
Carles F. Julià <carles.fernandez@upf.edu>
2013-03-19 18:52:01 +0100

    working on testing composition issues

---
+++ Apps/TestComposition/RecognizerTT_Test.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from GestureAgents.Recognizer import Recognizer, newHypothesis
+from GestureAgents.Events import Event
+from GestureAgents.Agent import Agent
+import math
+from GestureAgentsTUIO.Gestures2D.RecognizerTap import RecognizerTap
+
+
+def build_and_register_TT(RTKlass=RecognizerTap):
+
+    class RecognizerDT_Test(Recognizer):
+        newAgent = Event()
+        rtotal = 0
+
+        def __init__(self):
+            Recognizer.__init__(self)
+            self.RTKlass = RTKlass
+            self.agent = None
+            self.firstap = None
+            self.secondtap = None
+            self.register_event(
+                self.RTKlass.newAgent, RecognizerDT_Test.EventNewAgent)
+            self.time = 0.3
+            self.maxd = 10
+
+        @newHypothesis
+        def EventNewAgent(self, Tap):
+            self.agent = self.make_DoubleTapAgent()
+            self.agent.pos = Tap.pos
+            self.newAgent.call(self.agent)
+            if not self.agent.is_someone_subscribed():
+                self.fail(cause="Noone Interested")
+            else:
+                self.unregister_event(self.RTKlass.newAgent)
+                self.register_event(Tap.newTap, RecognizerDT_Test.FirstTap)
+
+        def FirstTap(self, Tap):
+            self.firstap = Tap
+            self.unregister_event(Tap.newTap)
+            self.register_event(
+                self.RTKlass.newAgent, RecognizerDT_Test.EventNewAgent2)
+            self.expire_in(self.time)
+            self.acquire(Tap)
+
+        @newHypothesis
+        def EventNewAgent2(self, Tap):
+            if self.dist(Tap.pos, self.firstap.pos) > self.maxd:
+                self.fail(cause="Max distance")
+            else:
+                self.unregister_event(self.RTKlass.newAgent)
+                self.register_event(Tap.newTap, RecognizerDT_Test.SecondTap)
+
+        def SecondTap(self, Tap):
+            self.secondtap = Tap
+            self.unregister_event(Tap.newTap)
+            self.register_event(
+                self.RTKlass.newAgent, RecognizerDT_Test.EventNewAgent3)
+            self.expire_in(self.time)
+            self.acquire(Tap)
+
+        @newHypothesis
+        def EventNewAgent3(self, Tap):
+            if self.dist(Tap.pos, self.secondtap.pos) > self.maxd:
+                self.fail(cause="Max distance")
+            else:
+                self.unregister_event(self.RTKlass.newAgent)
+                self.register_event(Tap.newTap, RecognizerDT_Test.SecondTap)
+
+        def ThirdTap(self, Tap):
+            if self.dist(Tap.pos, self.firstap.pos) > self.maxd:
+                self.fail(cause="Max distance")
+            else:
+                self.thirdtap = Tap
+                self.unregister_event(Tap.newTap)
+                self.cancel_expire()
+                self.acquire(Tap)
+                self.complete()
+                #print "I win",self
+                #print self.agent.newDoubleTap.registered
+                #import pdb; pdb.set_trace()
+                self.fail_all_others()
+
+        def execute(self):
+            #print "I execute",self
+            self.agent.pos = self.thirdtap.pos
+            #print self.agent.newDoubleTap.registered
+            self.agent.newDoubleTap(self.agent)
+            self.finish()
+
+        def duplicate(self):
+            d = self.get_copy()
+            d.firstap = self.firstap
+            d.secondtap = self.secondtap
+            d.thirdtap = self.thirdtap
+            return d
+
+        @staticmethod
+        def dist(a, b):
+            dx, dy = (a[0] - b[0], a[1] - b[1])
+            return math.sqrt(dx ** 2 + dy ** 2)
+
+        def make_DoubleTapAgent(self):
+            a = Agent(("newDoubleTap",), self)
+            return a
+
+        def __repr__(self):
+            return self.name
+
+    import GestureAgents.Gestures as Gestures
+    Gestures.load_recognizer(RecognizerDT_Test)
+    return RecognizerDT_Test
--- Apps/TestComposition/test_composition.py
+++ Apps/TestComposition/test_composition.py
@@ -1,3 +1,98 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from GestureAgents.AppRecognizer import AppRecognizer
+import GestureAgents.Gestures as Gestures
+from RecognizerDT_Test import build_and_register_DT
+from GestureAgents.Agent import Agent
+import GestureAgents.Reactor as Reactor
+
+
+events_3tap = [
+    ('f', 1, (0.5, 0.5), (
+     (0, 'in'),
+     (0.1, 'out'))),
+    ('f', 2, (0.5, 0.5), (
+     (0.2, 'in'),
+     (0.3, 'out'))),
+    ('f', 3, (0.5, 0.5), (
+     (0.4, 'in'),
+     (0.5, 'out'))),
+]
+
+
+def printf(s):
+    print s
+
+
+def test_events(evlist):
+    from GestureAgentsTUIO.Tuio import TuioAgentGenerator
+    maxtime = 0
+    for entry in evlist:
+        t = entry[0]
+        if t == 'f':
+            sid, pos, events = entry[1:]
+            a = TuioAgentGenerator.makeCursorAgent()
+            a.pos = pos
+            a.posx = pos[0]
+            a.posy = pos[1]
+            a.sessionid = sid
+            a.xmot = 0
+            a.ymot = 0
+            a.mot_accel = 0
+            for e in events:
+                time, ev_type = e[:2]
+                maxtime = max(maxtime, time)
+                if ev_type == 'in':
+                    f = lambda s, a=a: (a.newAgent(a), a.newCursor(a))
+                elif ev_type == 'out':
+                    f = lambda s, a=a: (a.removeCursor(a), a.finish())
+                else:
+                    raise NotImplementedError
+                Reactor.schedule_after(time, None, f)
+    return maxtime+1
+
+# Test Composition sharing sub-gestures
+#Test 1: 2 recognizers share a sub recognzer (Tap)
+
+
+RecognizerDT1 = build_and_register_DT()
+
+
+class AppTestComposition1(object):
+    def __init__(self):
+        AppRecognizer(RecognizerDT1).newAgent.register(AppTestComposition1.newAgent2Tap, self)
+        self.dtaps = 0
+
+    def newAgent2Tap(self, agent):
+        agent.newDoubleTap.register(AppTestComposition1.DT, self)
+
+    def DT(self, agent):
+        self.dtaps += 1
+
+
+#super simple Loop
+
+def run_apps(maxtime):
+    if Gestures.recognizers_loaded:
+        print "Loaded %d gesture recognizers:" % len(Gestures.recognizers)
+        for r in Gestures.recognizers:
+            print "\t%s" % str(r)
+
+    print "=" * 5 + "Agent.completion_policy Policy rules:" + "=" * 5
+    print Agent.completion_policy
+    running = [True]
+
+    def stop(a):
+        running[0] = False
+    Reactor.schedule_after(maxtime, None, stop)
+    while running[0]:
+        Reactor.run_all_now()
+
+if __name__ == '__main__':
+    appt1 = AppTestComposition1()
+    run_apps(test_events(events_3tap))
+    assert appt1.dtaps == 1
+    appt1 = AppTestComposition1()
+    run_apps(test_events(events_3tap))
+    assert appt1.dtaps == 1
 
