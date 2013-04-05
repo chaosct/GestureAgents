408538e74cb1ca5a4aec590ad835104bfad358b7
Carles F. Julià <carles.fernandez@upf.edu>
2013-04-05 19:58:53 +0200

    reactivating tests for RecognizerT_Test

--- Apps/TestComposition/test_RecognizerT.py
+++ Apps/TestComposition/test_RecognizerT.py
@@ -1,14 +1,14 @@
-# #Testing
+#Testing
 
-# from RecognizerT_Test import RecognizerT_Test
-# from recognizer_testing import t_r
+from RecognizerT_Test import RecognizerT_Test
+from recognizer_testing import t_r
 
-# events_tap = [
-#     ('f', 1, (0.5, 0.5), (
-#      (0, 'in'),
-#      (0.1, 'out'))),
-# ]
+events_tap = [
+    ('f', 1, (0.5, 0.5), (
+     (0, 'in'),
+     (0.1, 'out'))),
+]
 
 
-# class TapTestCase2(t_r(events_tap, RecognizerT_Test, ("newTap",), 1)):
-#     pass
+class TapTestCase2(t_r(events_tap, RecognizerT_Test, ("newTap",), 1)):
+    pass
