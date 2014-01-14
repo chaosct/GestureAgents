ccff3a5e9514bac2118a90decc8ef79d61b26f23
Carles F. Julià <carles.fernandez@upf.edu>
2014-01-14 10:59:34 +0100

    work on demoapp

--- Apps/DemoApp/DemoApp.py
+++ Apps/DemoApp/DemoApp.py
@@ -25,13 +25,14 @@ from GestureAgentsTUIO.Gestures2D.RecognizerDoubleTap import RecognizerDoubleTap
 
 class ShellApp(object):
     """docstring for ShellApp"""
-    def __init__(self):
+    def __init__(self, system):
         self.possible_apps = [DemoApp]
         self.running = {}
         self.maxid = 0
-        AppRecognizer(RecognizerDoubleTap).newAgent.register(
+        AppRecognizer(system, RecognizerDoubleTap).newAgent.register(
             ShellApp.newAgentDoubleTap, self)
         self.minimized = False
+        self.system = system
 
     def launch(self, App):
         id = self.maxid
@@ -81,17 +82,18 @@ class DemoApp(object):
 
 
 if __name__ == '__main__':
-    import GestureAgentsDemo
+    from GestureAgentsDemo import System
     from apps.Map import DemoMapApp
     from apps.Shadows import FingerShadow
     from apps.Pictures import AppPictureBrowser
     from apps.Calibrator import CalibratorApp
     # from apps.DebugRecognizers import DebugRecognizer
-    shell = ShellApp()
+    system = System()
+    shell = ShellApp(system)
     shell.launch(DemoMapApp)
     shell.launch(AppPictureBrowser)
     shell.launch(FingerShadow)
     shell.launch(CalibratorApp)
     # shell.launch(DebugRecognizer)
     # shell.launch(DemoApp)
-    GestureAgentsDemo.run_apps()
+    system.run_apps()
--- GestureAgentsDemo/__init__.py
+++ GestureAgentsDemo/__init__.py
@@ -5,26 +5,24 @@ import pyglet.app
 
 from GestureAgents import Gestures, Agent
 from Window import DemoWindow
+import GestureAgents.System
 
+class System(GestureAgents.System.System):
 
-def run_apps(debugMem=False):
-    global window
-    if Gestures.recognizers_loaded:
-        print "Loaded %d gesture recognizers:" % len(Gestures.recognizers)
-        for r in Gestures.recognizers:
-            print "\t%s" % str(r)
+    def run_apps(self, debugMem=False):
+        if Gestures.recognizers_loaded:
+            print "Loaded %d gesture recognizers:" % len(Gestures.recognizers)
+            for r in Gestures.recognizers:
+                print "\t%s" % str(r)
 
-    print "=" * 5 + " Agent.completion_policy Policy rules " + "=" * 5
-    print Agent.Agent.completion_policy
-    print "=" * 5 + " Agent.compatibility_policy Policy rules " + "=" * 5
-    print Agent.Agent.compatibility_policy
+        print "=" * 5 + " Agent.completion_policy Policy rules " + "=" * 5
+        print Agent.Agent.completion_policy
+        print "=" * 5 + " Agent.compatibility_policy Policy rules " + "=" * 5
+        print Agent.Agent.compatibility_policy
 
-    window = DemoWindow()
-    pyglet.app.run()
+        self.window = DemoWindow()
+        pyglet.app.run()
 
+    def stop(self):
+        pyglet.app.exit()
 
-def run():
-    run_apps()
-
-if __name__ == '__main__':
-    run()
